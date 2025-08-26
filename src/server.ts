bad326e5b7c108341c48387a73bfdab4a5bc0b9d
Siful Islam <mdsiful.islam@ugent.be>
2025-08-26 12:36:07 +0100

    server vercel deployment

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,21 @@
+import { Server } from "http";
 import mongoose from "mongoose";
-if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-}
+import "dotenv/config";
 import app from "./app";
 
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+let server: Server;
+const port = process.env.PORT || 9000;
 
-if (!process.env.MONGODB_URI) {
-    const user = process.env.DB_USER || "";
-    const pass = encodeURIComponent(process.env.DB_PASSWORD || "");
-    const db = process.env.DB_NAME || "test";
-    process.env.MONGODB_URI = `mongodb+srv://${user}:${pass}@cluster0.vp2mu5v.mongodb.net/${db}?retryWrites=true&w=majority&appName=Cluster0`;
-}
-
-async function connectDB() {
+async function main() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI as string);
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vp2mu5v.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`);
         console.log("Connected to MongoDB");
+        server = app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
     } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
+        console.error("Error starting the server:", error);
         process.exit(1);
     }
 }
-
-connectDB();
-
-if (process.env.NODE_ENV !== "production") {
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
-}
-
-export default app;
+main();

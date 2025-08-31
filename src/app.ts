c89080d3d0ba3a82822ab891eb6b1e213f9388bc
Siful Islam <mdsiful.islam@ugent.be>
2025-08-31 17:18:00 +0100

    cors origin link update

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: ["", "http://localhost:5173"],
+        origin: ["https://library-management-api-xjyd.vercel.app/api", "http://localhost:5173"],
     })
 );
 
